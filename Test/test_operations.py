need to fix broadcasting backward pass

Test/test_operations.py
@@ -37,7 +37,8 @@ class TestOps(unittest.TestCase):
     def test_matmul(self):
         helper_test([(45, 65) ,(65, 45)] , lambda x ,y : x @ y , lambda x ,y : x @ y)
         helper_test([(45, 65) ,(65, 1)] , lambda x ,y : x @ y , lambda x ,y : x @ y)
-    # sum  and reduce
+        helper_test([(45, 1 ) ,(1, 1)] , lambda x ,y : x @ y , lambda x ,y : x @ y)
+    # sum  and 
     def test_sum(self):
         helper_test([(45 , 65)] , torch.sum  , Tensor.Sum)
         helper_test([(45 , 65)] , lambda x : x.sum(dim = 0)   , lambda x:x.Sum(dim = 0))

projekatgrad/Tensor.py
@@ -1,20 +1,20 @@
 import numpy as np 
 from functools import partialmethod 
 # make a decent numpy based NN library 
-# then add GPU support
+# then add GPU support 
 
 class  Function:
-    def __init__(self , *tensors , **kwargs):
-        self.parents  = tuple([x for x in tensors if isinstance(x ,Tensor)]) # bad fix this register is not good#tensors #
+    def __init__(self , *tensors):
+        self.parents  = tuple([x for x in tensors if isinstance(x ,Tensor)]) # not  ideal
         #print(type(self.parents))
         self.saved_tensors = []
     
     def save_for_backward(self, *tensors):  
         self.saved_tensors.extend(tensors)
 
-    def apply(self , func , *x , **kwargs):
+    def apply(self , func , *x):
         ctx = func(self, *x)
-        ret = Tensor(func.forward(ctx , self.data  , *[t.data if isinstance(t, Tensor) else t for t in x] , **kwargs))  #this  is shit
+        ret = Tensor(func.forward(ctx , self.data  , *[t.data if isinstance(t, Tensor) else t for t in x])) # returns Tensor but args  are  np.array
         ret._ctx = ctx
         return ret
     
@@ -22,14 +22,14 @@ class  Tensor:
     def __init__(self, data , requires_grad = False):
     
         self.grad ,self._ctx = None , None
-        self.requires_grad  = requires_grad
+        self.requires_grad  = requires_grad # if any tensor  requres_grad then  do  backpass 
         if isinstance(data, (list , tuple , int , float)):
             data = np.array(data , dtype = np.float32)
         if isinstance(data, np.ndarray): 
             data = data.astype(np.float32) 
 
         if not isinstance(data ,(np.ndarray , np.generic)):
-            raise RuntimeError (f"Can't create a tensor from {data}")
+            raise RuntimeError (f"Can't create a tensor from {data , type(data)}")
         
         self.data = data 
 
@@ -65,27 +65,27 @@ class  Tensor:
                     ts.grad = gr if ts.grad is None else (ts.grad + gr)
             i._ctx = None
 
-    # for binary ?
-    def assure_tensor(self , x , func =  None  , reversed = False):
+    # maybe  not  ideal
+    def assure_tensor(self , x, func =  None  , reversed = False):
           
         self = self if isinstance(self , Tensor) else Tensor(self)
         x =  x if isinstance(x , Tensor) else Tensor(x)
         return func(x, self) if reversed else func(self, x)      
    
-    # fundamental(sub can be kicked)
+    # fundamental
     def Mul(self, x  ,reversed = False):  return self.assure_tensor(x, Tensor.mul ,reversed)     
     def Add(self, x  ,reversed = False): return self.assure_tensor(x, Tensor.add ,reversed)
-    def Sub(self, x , reversed = False): return self.assure_tensor(x, Tensor.sub ,reversed)
     def Pow(self, x , reversed = False): return self.assure_tensor(x, Tensor.pow ,reversed) 
-    def Div(self, x , reversed = False): return self.assure_tensor(x, Tensor.div ,reversed)
-    def Exp(self, x , reversed  = False):  return self.assure_tensor(None , Tensor.exp ,reversed)
-    def Matmul(self,x): return  self.dot(x) # dot matmul
+    def Exp(self):  return self.exp() # assumes  self  is a tensor
+
     # basic math
+    def Div(self, x , reversed = False): return self * x ** -1 if not reversed else x * self**-1 # this is  not  ideal
+    def Sub(self,x , reversed = False): return self + (-x) if not reversed else x + (-self)
     def Log(self):return self.log()        
     def Sqrt(self): return self.pow(0.5)
     def Neg(self):  return self.mul(-1)
-   
-
+    def Matmul(self,x): return  self.dot(x) 
+    
     def __add__(self, x): return self.Add(x)
     def __sub__(self, x): return self.Sub(x)
     def __mul__(self, x): return self.Mul(x)
@@ -116,7 +116,7 @@ class  Tensor:
         out = exp / s 
         return out
     
-def register(name , func):
+def register(name , func): # this  is called n number of times (n  is tyhe number  of fucntins)
     partial = partialmethod(func.apply , func)
     setattr(Tensor , name , partial)
 

projekatgrad/ops.py
@@ -1,11 +1,11 @@
-from projekatgrad.Tensor import Function, register
+from projekatgrad.Tensor import Tensor, Function, register
 import  numpy as  np
 
-# dot  can be definig as  a conv 
+# dot  can be definig as  a conv(
 # broadcasting is fckd ! 
-
+# only  fundamental ops
 def unbroad_reshape(x, desired_shape): # eithher  sum  or rehape
-    
+    print(x)
     if max(x.shape) > max(desired_shape):
         x = np.array([np.sum(x)])
         if x.shape != desired_shape:
@@ -21,16 +21,10 @@ class dot(Function):
         out = x @ y 
         ctx.save_for_backward(x,y) 
         return out
-         
     @staticmethod
-    def backward(ctx, out_grad): # TODO :make a nicer  way  of doing this
+    def backward(ctx, out_grad): 
         x,y= ctx.saved_tensors
-        t1 = np.expand_dims(x, 0) if x.ndim < 2 else x # za  Y.grad
-        out_grad_1 = np.expand_dims(out_grad , 0) if out_grad.ndim < 2 else out_grad
-        t2 = np.expand_dims(y  ,-1) if y.ndim < 2 else y
-        out1 = (out_grad_1 @ t2.T).reshape(x.shape) # bad
-        out2 = (t1.T @ out_grad_1).reshape(y.shape) # bad
-        return  unbroad_reshape(out1 , x.shape) , unbroad_reshape(out2 ,y.shape)
+        return  unbroad_reshape(out_grad @ y.T , x.shape) , unbroad_reshape(x.T @ out_grad ,y.shape)
 register("dot",dot)
 
 class mul(Function):
@@ -44,16 +38,6 @@ class mul(Function):
         return unbroad_reshape(y * out_grad , x.shape) , unbroad_reshape(x * out_grad , y.shape)
 register("mul",mul) 
 
-class div(Function):
-    def forward(ctx,x,y): 
-        ctx.save_for_backward(x,y)
-        return x/y
-    @staticmethod
-    def backward(ctx, out_grad):
-        x,y  = ctx.saved_tensors
-        return unbroad_reshape(1/y * out_grad , x.shape) , unbroad_reshape(-(x/y**2) * out_grad , y.shape) 
-register("div",div)
-
 class add(Function):
     @staticmethod 
     def forward(ctx,x,y):
@@ -63,16 +47,6 @@ class add(Function):
     def backward(ctx, out_grad):
         return out_grad , out_grad
 register("add",add)
-
-class sub(Function):
-    @staticmethod 
-    def forward(ctx,x,y):
-        ctx.save_for_backward(x,y)
-        return x-y
-    @staticmethod
-    def backward(ctx, out_grad):
-        return out_grad , -out_grad
-register("sub",sub)
 
 class pow(Function):
     @staticmethod